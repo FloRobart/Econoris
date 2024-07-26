8b45a97896cc33719d051b66ecbf4812e12e6361
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-26 23:07:51 +0200

    correction de bug mineur

--- resources/views/private/investissement.blade.php
+++ resources/views/private/investissement.blade.php
@@ -148,12 +148,14 @@
                             
                             <!-- Actions -->
                             <td class="smallRowCenterContainer px-1 min-[460px]:px-2 min-[500px]:px-4 py-2">
-                                <!-- Modifier -->
-                                <button onclick="editInvestissement('{{ strftime('%Y-%m-%d', strtotime($investissement->date_transaction)) }}', '{{ str_replace('\'', '\\\'', $investissement->nom_actif) }}', '{{ $investissement->montant_transaction }}', '{{ $investissement->frais_transaction }}', '{{ $investissement->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                    </svg>
-                                </button>
+                                @if (!str_contains(strtolower(URL::current()), 'type/investissements'))
+                                    <!-- Modifier -->
+                                    <button onclick="editInvestissement('{{ strftime('%Y-%m-%d', strtotime($investissement->date_transaction)) }}', '{{ str_replace('\'', '\\\'', $investissement->nom_actif) }}', '{{ $investissement->montant_transaction }}', '{{ $investissement->frais_transaction }}', '{{ $investissement->id }}')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgBleuLogo hover:bgBleuFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="tinySizeIcons">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+                                @endif
 
                                 <!-- Supprimer -->
                                 <a href="{{ route('investissement.remove', $investissement->id) }}" onclick="return confirm('Êtes-vous sûr de vouloir supprimer l\'investissement en {{ str_replace('\'', '\\\'', $investissement->type_investissement_investissement) }} du {{ strftime('%A %d %B %Y',strtotime($investissement->date_transaction)) }} ? Cette action est irréversible.')" class="smallRowCenterContainer w-fit smallTextReverse font-bold bgError hover:bgErrorFonce focus:normalScale rounded-lg min-[500px]:rounded-xl py-1 px-1 min-[500px]:px-2 ml-1 min-[500px]:ml-2">
@@ -169,23 +171,25 @@
         </table>
 
         <!-- Formulaire pour ajouter un investissement -->
-        <form id="form" action="{{ route('investissement.add') }}" method="POST" class="rowStartContainer hidden">
-            @csrf
-            <div class="colCenterContainer">
-                <div class="colStartContainer md:rowStartContainer">
-                    <input id="type_investissement" name="type_investissement" required type="hidden" value="{{ $type_investissement }}">
-                    <input id="date_transaction"    name="date_transaction"    required type="date" value="{{ date('Y-m-d') }}" max="{{ date('Y-m-d') }}" class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input id="nom_actif"           name="nom_actif"           required type="text" placeholder="Nom de l'actif"                          class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input id="montant_transaction" name="montant_transaction" required type="number" step="0.01" placeholder="Montant investie" min="0"  class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
-                    <input id="frais_transaction"   name="frais_transaction"   required type="number" step="0.01" placeholder="Montant des frais" min="0" class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+        @if (!str_contains(strtolower(URL::current()), 'type/investissements'))
+            <form id="form" action="{{ route('investissement.add') }}" method="POST" class="rowStartContainer hidden">
+                @csrf
+                <div class="colCenterContainer">
+                    <div class="colStartContainer md:rowStartContainer">
+                        <input id="type_investissement" name="type_investissement" required type="hidden" value="{{ $type_investissement }}">
+                        <input id="date_transaction"    name="date_transaction"    required type="date" value="{{ date('Y-m-d') }}" max="{{ date('Y-m-d') }}" class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                        <input id="nom_actif"           name="nom_actif"           required type="text" placeholder="Nom de l'actif"                          class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                        <input id="montant_transaction" name="montant_transaction" required type="number" step="0.01" placeholder="Montant investie" min="0"  class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                        <input id="frais_transaction"   name="frais_transaction"   required type="number" step="0.01" placeholder="Montant des frais" min="0" class="w-[55%] mx-2 min-[500px]:mx-4 my-2 text-center inputForm smallText">
+                    </div>
+                    <button id="formButton" class="buttonForm mx-2 min-[500px]:mx-4 my-2">Ajouter</button>
+                    <div class="w-full tableRowTop"></div>
                 </div>
-                <button id="formButton" class="buttonForm mx-2 min-[500px]:mx-4 my-2">Ajouter</button>
-                <div class="w-full tableRowTop"></div>
-            </div>
-        </form>
+            </form>
 
-        <!-- Bouton pour ajouter un investissement -->
-        <button onclick="showForm('Ajouter un investissement', 'Ajouter', '{{ route('investissement.add') }}')" id="button" class="buttonForm mt-8">Ajouter un investissement</a>
+            <!-- Bouton pour ajouter un investissement -->
+            <button onclick="showForm('Ajouter un investissement', 'Ajouter', '{{ route('investissement.add') }}')" id="button" class="buttonForm mt-8">Ajouter un investissement</a>
+        @endif
     </div>
 </section>
 @endsection
@@ -215,6 +219,7 @@
             document.getElementById('id').remove();
         }
         document.getElementById('form').insertAdjacentHTML('beforeend', '<input type="hidden" id="id" name="id" value="' + id + '">');
+        document.getElementById('form').scrollIntoView();
 
         oldId = id;
     }
