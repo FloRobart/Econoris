9f2fe815c0b076cbae59dd106b00ed417820e009
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-27 12:16:22 +0200

    suppression d'un import inutilisé

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -11,7 +11,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
 
 
 class ProfilController extends Controller
